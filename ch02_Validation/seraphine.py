ee6e621f5df22be55654dc6d11f0e9256e42cd86
Seraphine Young <seraphine_ene@yahoo.com>
2019-01-23 14:07:44 +0000

    working on task 6

--- ch02_Validation/seraphine.py
+++ ch02_Validation/seraphine.py
@@ -13,13 +13,43 @@
     
 ################################# Task 5 ################################
 
+#print('***choice***') 
+#print('1.Display my name') 
+#print('2. Display my age')
+#print('3. Display my city')
+#
+#
+#choice = 0
+#
+##this puts it in a loop
+#
+#while True:
+#    
+##   choice = int(input('What is your choice? '))
+#    
+#    
+#   while choice < 1 or choice > 3:
+#       choice = int(input('what is your choice again? '))
+#    
+#       if choice == 1:
+#            print('Ms Wu')
+#            
+#        
+#       elif choice == 2:
+#            print('5 years old')
+#            
+#        
+#       elif choice == 3:
+#            print('London')
+#   break
+
+    
+  ################################# Task 6 ################################  
+    
 print('***choice***') 
 print('1.Display my name') 
 print('2. Display my age')
 print('3. Display my city')
-
-
-
 
 
 choice = 0
@@ -27,31 +57,35 @@ choice = 0
 #this puts it in a loop
 
 while True:
-    
-#   choice = int(input('What is your choice? '))
-    
-    
-   while choice < 1 or choice > 3:
-       choice = int(input('what is your choice again? '))
-    
-       if choice == 1:
-            print('Ms Wu')
-            
+    try:
+
+       while choice < 1 or choice > 3:
+           choice = int(input('what is your choice again? '))
+           
+           if choice == 1:
+               print('Ms Wu')
         
-       elif choice == 2:
-            print('5 years old')
-            
+    
+           elif choice == 2:
+               print('5 years old')
         
-       elif choice == 3:
-            print('London')
-   break
+    
+           elif choice == 3:
+               print('London')
+                
+           
+           
+       break
+   
+    except ValueError:
+         print('Please enter a number')
+         
+#    else:
+        
+    
 
     
-    
-    
-        
-    
-
+            
 
 
 
